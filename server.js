b66e2e0f624cfd77bb106a39dc11c4039283e272
shailendra singh kanshana <97683040+shailendrakanshana@users.noreply.github.com>
2023-06-06 00:31:11 +0530

    Updates

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const app=express();
 require('dotenv').config();
-const port=process.env.PORT || 3000;
+const port=process.env.PORT || 8000;
 const mongo=require('mongoose');
 var session = require('express-session')
 const auth = require('./routes/auth');
